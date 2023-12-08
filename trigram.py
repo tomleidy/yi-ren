eaf51925dd4d9bc1898353676a76d89617522214
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-12-07 21:57:57 -0800

    chore: changed single quotes to double quotes

--- trigram.py
+++ trigram.py
@@ -8,7 +8,7 @@ trigram_lines_to_trigram_pinyin = {
 
 
 class Trigram:
-    """We have our lines, let's get our trigram!"""
+    """We have our lines, time to get our trigram!"""
 
     def __init__(self, lines: list):
         self.original_line_values = []
@@ -40,22 +40,22 @@ if __name__ == "__main__":
         (7, 8, 8): "Xun", (7, 8, 7): "Kan", (7, 7, 8): "Gen", (7, 7, 7): "Kun"
     }
     expected_serialized_trigrams = {
-        (9, 9, 9): {'values': [8, 8, 8], 'moving': True, 'original_values': [9, 9, 9], 'pinyin': 'Qian'},
-        (9, 9, 6): {'values': [8, 8, 7], 'moving': True, 'original_values': [9, 9, 6], 'pinyin': 'Dui'},
-        (9, 6, 9): {'values': [8, 7, 8], 'moving': True, 'original_values': [9, 6, 9], 'pinyin': 'Li'},
-        (9, 6, 6): {'values': [8, 7, 7], 'moving': True, 'original_values': [9, 6, 6], 'pinyin': 'Zhen'},
-        (6, 9, 9): {'values': [7, 8, 8], 'moving': True, 'original_values': [6, 9, 9], 'pinyin': 'Xun'},
-        (6, 9, 6): {'values': [7, 8, 7], 'moving': True, 'original_values': [6, 9, 6], 'pinyin': 'Kan'},
-        (6, 6, 9): {'values': [7, 7, 8], 'moving': True, 'original_values': [6, 6, 9], 'pinyin': 'Gen'},
-        (6, 6, 6): {'values': [7, 7, 7], 'moving': True, 'original_values': [6, 6, 6], 'pinyin': 'Kun'},
-        (8, 8, 8): {'values': [8, 8, 8], 'moving': False, 'original_values': [8, 8, 8], 'pinyin': 'Qian'},
-        (8, 8, 7): {'values': [8, 8, 7], 'moving': False, 'original_values': [8, 8, 7], 'pinyin': 'Dui'},
-        (8, 7, 8): {'values': [8, 7, 8], 'moving': False, 'original_values': [8, 7, 8], 'pinyin': 'Li'},
-        (8, 7, 7): {'values': [8, 7, 7], 'moving': False, 'original_values': [8, 7, 7], 'pinyin': 'Zhen'},
-        (7, 8, 8): {'values': [7, 8, 8], 'moving': False, 'original_values': [7, 8, 8], 'pinyin': 'Xun'},
-        (7, 8, 7): {'values': [7, 8, 7], 'moving': False, 'original_values': [7, 8, 7], 'pinyin': 'Kan'},
-        (7, 7, 8): {'values': [7, 7, 8], 'moving': False, 'original_values': [7, 7, 8], 'pinyin': 'Gen'},
-        (7, 7, 7): {'values': [7, 7, 7], 'moving': False, 'original_values': [7, 7, 7], 'pinyin': 'Kun'}
+        (9, 9, 9): {"values": [8, 8, 8], "moving": True, "original_values": [9, 9, 9], "pinyin": "Qian"},
+        (9, 9, 6): {"values": [8, 8, 7], "moving": True, "original_values": [9, 9, 6], "pinyin": "Dui"},
+        (9, 6, 9): {"values": [8, 7, 8], "moving": True, "original_values": [9, 6, 9], "pinyin": "Li"},
+        (9, 6, 6): {"values": [8, 7, 7], "moving": True, "original_values": [9, 6, 6], "pinyin": "Zhen"},
+        (6, 9, 9): {"values": [7, 8, 8], "moving": True, "original_values": [6, 9, 9], "pinyin": "Xun"},
+        (6, 9, 6): {"values": [7, 8, 7], "moving": True, "original_values": [6, 9, 6], "pinyin": "Kan"},
+        (6, 6, 9): {"values": [7, 7, 8], "moving": True, "original_values": [6, 6, 9], "pinyin": "Gen"},
+        (6, 6, 6): {"values": [7, 7, 7], "moving": True, "original_values": [6, 6, 6], "pinyin": "Kun"},
+        (8, 8, 8): {"values": [8, 8, 8], "moving": False, "original_values": [8, 8, 8], "pinyin": "Qian"},
+        (8, 8, 7): {"values": [8, 8, 7], "moving": False, "original_values": [8, 8, 7], "pinyin": "Dui"},
+        (8, 7, 8): {"values": [8, 7, 8], "moving": False, "original_values": [8, 7, 8], "pinyin": "Li"},
+        (8, 7, 7): {"values": [8, 7, 7], "moving": False, "original_values": [8, 7, 7], "pinyin": "Zhen"},
+        (7, 8, 8): {"values": [7, 8, 8], "moving": False, "original_values": [7, 8, 8], "pinyin": "Xun"},
+        (7, 8, 7): {"values": [7, 8, 7], "moving": False, "original_values": [7, 8, 7], "pinyin": "Kan"},
+        (7, 7, 8): {"values": [7, 7, 8], "moving": False, "original_values": [7, 7, 8], "pinyin": "Gen"},
+        (7, 7, 7): {"values": [7, 7, 7], "moving": False, "original_values": [7, 7, 7], "pinyin": "Kun"}
     }
     print("Unit tests for trigram.py")
     from unittests import assert_equal
