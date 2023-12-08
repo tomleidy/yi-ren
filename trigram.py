fbc868c531e6854955b2f7f659f1fa1f8217e14b
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-12-07 19:16:17 -0800

    refactor: now uses Line class, also shortened

--- trigram.py
+++ trigram.py
@@ -1,84 +1,72 @@
 """Trigram"""
-from yijing_references import trigram_lines_to_trigram_pinyin
+
+
+trigram_lines_to_trigram_pinyin = {
+    (8, 8, 8): "Qian", (8, 8, 7): "Dui", (8, 7, 8): "Li", (8, 7, 7): "Zhen",
+    (7, 8, 8): "Xun", (7, 8, 7): "Kan", (7, 7, 8): "Gen", (7, 7, 7): "Kun"
+}
 
 
 class Trigram:
     """We have our lines, let's get our trigram!"""
 
-    def __init__(self, line_values):
-        self.line_values = line_values
-        self._is_moving = 6 in line_values or 9 in line_values
-        self.name_current = self._lookup_name_current(line_values)
-        self.moving_lines = self._identify_moving_lines(line_values)
+    def __init__(self, lines: list):
+        self.original_line_values = []
+        self.line_values = []
+        self.moving = False
+        for line in lines:
+            self.original_line_values.append(line.original)
+            self.line_values.append(line.value)
+            if line.moving:
+                self.moving = True
+        self.pinyin = trigram_lines_to_trigram_pinyin[tuple(
+            self.line_values)]
 
-        self.name_alternate = self._lookup_name_alternate(line_values)
-
-    def _lookup_name_current(self, line_values: list):
-        if self._is_moving:
-            lookup_values_current = self._create_lookup_values_current(
-                line_values)
-            return self._lookup_trigram_name(lookup_values_current)
-        return self._lookup_trigram_name(tuple(line_values))
-
-    def _lookup_name_alternate(self, line_values: list):
-        if self._is_moving:
-            lookup_values_alternate = self._create_lookup_values_alternate(
-                line_values)
-            return self._lookup_trigram_name(lookup_values_alternate)
-        return ""
-
-    def __repr__(self):
-        return f"Trigram(line_values={self.line_values})"
-
-    def __str__(self):
-        text = f"{self.name_current}"
-        if self.name_alternate != "":
-            text += f" -> {self.name_alternate}, "
-            text += f"values: {self.line_values}, "
-            text += f"moving lines: {self.moving_lines}"
-        else:
-            text += f", values: {self.line_values}"
-        return text
-
-    def _identify_moving_lines(self, line_values):
-        """Identify and return the line numbers of the moving lines."""
-        return [idx+1 for idx, value in enumerate(line_values) if value in [6, 9]]
-
-    def _calculate_moving_lines(self, line_values):
-        """Create a list of booleans indicating whether each line is moving."""
-        return [line in [6, 9] for line in line_values]
-
-    def _create_lookup_values_current(self, line_values: list):
-        """Convert line values from 6 to 7 or 9 to 8. 7 and 8 remain the same."""
-        return tuple(7 if x == 6 else 8 if x == 9 else x for x in line_values)
-
-    def _create_lookup_values_alternate(self, line_values: list):
-        """Convert line values from 6 to 8 and 9 to 7. 7 and 8 remain the same."""
-        return tuple(8 if x == 6 else 7 if x == 9 else x for x in line_values)
-
-    def _lookup_trigram_name(self, lookup_values: tuple):
-        """Set trigram name (pinyin without tone levels) based on trigram line values"""
-        return trigram_lines_to_trigram_pinyin[lookup_values]
-
-    def get_values(self):
-        """Return line_values"""
-        return self.line_values
-
-    def get_current(self):
-        """Return name of this trigram"""
-        return self.name_current
-
-    def get_alternate(self):
-        """Return name of alternate trigram"""
-        return self.name_alternate
-
-    def get_moving_lines(self):
-        """Return tuple of moving lines"""
-        return self.moving_lines
-
-    def has_alternate(self):
-        """Return whether or not this trigram is moving/has alternate trigram"""
-        return self._is_moving
+    def serialize(self):
+        """Get a dictionary for this trigram"""
+        return {
+            "values": self.line_values,
+            "moving": self.moving,
+            "original_values": self.original_line_values,
+            "pinyin": self.pinyin
+        }
 
 
-# To run once:
+if __name__ == "__main__":
+    test_values_trigram_lines = {
+        (9, 9, 9): "Qian", (9, 9, 6): "Dui", (9, 6, 9): "Li", (9, 6, 6): "Zhen",
+        (6, 9, 9): "Xun", (6, 9, 6): "Kan", (6, 6, 9): "Gen", (6, 6, 6): "Kun",
+        (8, 8, 8): "Qian", (8, 8, 7): "Dui", (8, 7, 8): "Li", (8, 7, 7): "Zhen",
+        (7, 8, 8): "Xun", (7, 8, 7): "Kan", (7, 7, 8): "Gen", (7, 7, 7): "Kun"
+    }
+    expected_serialized_trigrams = {
+        (9, 9, 9): {'values': [8, 8, 8], 'moving': True, 'original_values': [9, 9, 9], 'pinyin': 'Qian'},
+        (9, 9, 6): {'values': [8, 8, 7], 'moving': True, 'original_values': [9, 9, 6], 'pinyin': 'Dui'},
+        (9, 6, 9): {'values': [8, 7, 8], 'moving': True, 'original_values': [9, 6, 9], 'pinyin': 'Li'},
+        (9, 6, 6): {'values': [8, 7, 7], 'moving': True, 'original_values': [9, 6, 6], 'pinyin': 'Zhen'},
+        (6, 9, 9): {'values': [7, 8, 8], 'moving': True, 'original_values': [6, 9, 9], 'pinyin': 'Xun'},
+        (6, 9, 6): {'values': [7, 8, 7], 'moving': True, 'original_values': [6, 9, 6], 'pinyin': 'Kan'},
+        (6, 6, 9): {'values': [7, 7, 8], 'moving': True, 'original_values': [6, 6, 9], 'pinyin': 'Gen'},
+        (6, 6, 6): {'values': [7, 7, 7], 'moving': True, 'original_values': [6, 6, 6], 'pinyin': 'Kun'},
+        (8, 8, 8): {'values': [8, 8, 8], 'moving': False, 'original_values': [8, 8, 8], 'pinyin': 'Qian'},
+        (8, 8, 7): {'values': [8, 8, 7], 'moving': False, 'original_values': [8, 8, 7], 'pinyin': 'Dui'},
+        (8, 7, 8): {'values': [8, 7, 8], 'moving': False, 'original_values': [8, 7, 8], 'pinyin': 'Li'},
+        (8, 7, 7): {'values': [8, 7, 7], 'moving': False, 'original_values': [8, 7, 7], 'pinyin': 'Zhen'},
+        (7, 8, 8): {'values': [7, 8, 8], 'moving': False, 'original_values': [7, 8, 8], 'pinyin': 'Xun'},
+        (7, 8, 7): {'values': [7, 8, 7], 'moving': False, 'original_values': [7, 8, 7], 'pinyin': 'Kan'},
+        (7, 7, 8): {'values': [7, 7, 8], 'moving': False, 'original_values': [7, 7, 8], 'pinyin': 'Gen'},
+        (7, 7, 7): {'values': [7, 7, 7], 'moving': False, 'original_values': [7, 7, 7], 'pinyin': 'Kun'}
+    }
+    print("Unit tests for trigram.py")
+    from unittests import assert_equal
+    from line import Line
+
+    list_of_trigrams = []
+    for lines, trigram in test_values_trigram_lines.items():
+        line_list = []
+        for line in lines:
+            line_list.append(Line(line))
+        actual = Trigram(line_list).serialize()
+        expected = expected_serialized_trigrams[lines]
+        message = f"{trigram} serializes properly with {lines}"
+        assert_equal(actual, expected, message)
