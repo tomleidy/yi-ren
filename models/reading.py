42aeadd9563c8ca8571b20884c2320aace55a17a
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2023-12-12 19:38:02 -0800

    refactor: simpler reading model

--- models/reading.py
+++ models/reading.py
@@ -1,49 +1,48 @@
-from datetime import datetime, timezone
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+"""SQLAlchemy model for readings table"""
+from sqlalchemy import Column, Integer, DateTime, Text
 from database.base import Base
+from helpers import utc_ts
 
 
 class Reading(Base):
     """Handles SQLAlchemy model for readings table"""
 
-    def __init__(self, reader_id: int, client_id: int, hexagrams: list, moving_lines: list, topic: str = "", notes: str = ""):
-        self.reader_id = reader_id
-        self.client_id = client_id
-
-        self.hexagram_primary = hexagrams[0]
-        if len(hexagrams) > 1:
-            self.hexagram_secondary = hexagrams[1]
-        for i, line in enumerate(moving_lines, start=1):
-            setattr(self, f'line_moving_{i}', line)
-
-        self.topic = topic
-        self.reading_notes = notes
+    def __init__(self, hexagrams: dict, reading_info: dict = None):
+        if reading_info:
+            self.reader_id = reading_info.reader
+            self.client_id = reading_info.client
+            self.topic = reading_info.topic
+            self.reading_notes = reading_info.notes
+            self.hexagram_stationary = hexagrams["hexagram"]["stationary"]
 
     __tablename__ = "readings"
     reading_id = Column(Integer, primary_key=True, autoincrement=True)
-    # TODO: make reader_id and client_id nullable=False (requires more user table setup)
+    # TODO: add constraint linking reader_id and client_id to the users table.
+    # TODO: make reader_id and client_id nullable=False
+    reader_id = Column(Integer, nullable=True)
+    client_id = Column(Integer, nullable=True)
+    topic = Column(Text, nullable=False, default="")
+    reading_notes = Column(Text, nullable=True)
+    created_at = Column(DateTime, nullable=False, default=utc_ts())
+
+    hexagram_stationary = Column(Integer, nullable=False)
+    hexagram_moving = Column(Integer, nullable=True)
 
     def serialize(self):
         """Returns reading information in dictionary"""
-        moving_list = [
-            self.line_moving_1,
-            self.line_moving_2,
-            self.line_moving_3,
-            self.line_moving_4,
-            self.line_moving_5,
-            self.line_moving_6
-        ]
         reading_dict = {
-            "reading_id": self.reading_id,
-            "topic": self.topic,
-            "hexagram_primary": self.hexagram_primary,
-            "lines_moving": moving_list,
-            "created_at": self.created_at.isoformat(),
+            # metadata
             "reader_id": self.reader_id,
             "client_id": self.client_id,
-            "reading_notes": self.reading_notes
-
-
+            "created_at": self.created_at.isoformat(),
+            # reading data
+            "topic": self.topic,
+            "reading_notes": self.reading_notes,
+            "reading_id": self.reading_id,
+            "hexagrams": {
+                "stationary": self.hexagram_stationary,
+                "moving": self.hexagram_moving
+            },
         }
-        if self.hexagram_secondary:
-            reading_dict["hexagram_secondary"] = self.hexagram_secondary
+        # TODO: add call to fill_dictionary helper once it's implemented
+        return reading_dict
