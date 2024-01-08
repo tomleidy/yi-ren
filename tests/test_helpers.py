0b0fd236ae1f3e20f1627f77119e1321aac5fecd
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-01-07 22:28:49 -0800

    test: get_mutual_hexagram_from_hexagram_number

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,7 @@
 """Testing suite for helper functions"""
 import pytest  # pylint: disable=W0611
 from gua.helpers import get_moving_lines_from_hexagram_pairs, fill_reading_dictionary
+from gua.helpers import get_mutual_hexagram_from_hexagram_number
 
 
 def test_get_moving_lines_from_hexagram_pairs():
@@ -16,11 +17,11 @@ def test_fill_reading_dictionary():
     """Test fill_reading_dictionary"""
     hexagrams_to_test = {"hexagram_stationary": 1, "hexagram_moving": 2}
     result = fill_reading_dictionary(hexagrams_to_test)
-    assert 'moving_lines' in result
-    assert 'trigrams_stationary' in result
-    assert 'trigrams_moving' in result
-    assert 'line_values' in result
-    assert result['moving_lines'] == [1, 2, 3, 4, 5, 6]
-    assert result['trigrams_stationary'] == ["Qian", "Qian"]
-    assert result['trigrams_moving'] == ["Kun", "Kun"]
-    assert result['line_values'] == [9, 9, 9, 9, 9, 9]
+mutual_parameters = [(27, (7, 7, 7, 7, 7, 7)), (28, (8, 8, 8, 8, 8, 8))]
+
+
+@pytest.mark.parametrize("hexagram_int, lines_tuple", mutual_parameters)
+def test_get_mutual_hexagram_from_hexagram_number(hexagram_int, lines_tuple):
+    """"""
+    assert get_mutual_hexagram_from_hexagram_number(
+        hexagram_int) == lines_tuple
