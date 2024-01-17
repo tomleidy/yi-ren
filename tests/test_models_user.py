8b9bb0e79205e4c2f39795d025c5e4ae0f5d5a88
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-01-16 20:37:50 -0800

    test: testing for user model

---
+++ tests/test_models_user.py
@@ -0,0 +1,28 @@
+""""Testing suite for user models"""
+import pytest
+from models.user import User
+# from models import Session
+
+
+# def test_user_serialize():
+#     user = User()
+#     # query = session.query(User).filter(User.user_id == 1).first()
+#     result = query.serialize()
+#     assert result is not None
+#     assert result.username == 'defaultuser'
+
+def test_user_lookup_username():
+    """Validate User.lookup_username()"""
+    user = User()
+    result = user.lookup_username("defaultuser")
+    assert result['user_id'] == 1
+    assert result['chosen_personal_name'] == "default"
+    assert result['created_at']
+    assert result['last_modified']
+
+
+def test_user_get_all_users():
+    user = User()
+    result = user.get_all_users()
+    assert result[0].username == 'defaultuser'
+    assert result[0].user_id == 1
