bc36737541ce19e3c9ec8bd9694ba4735910d42c
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-01-28 23:52:17 -0800

    chore: session = Session -> with Session() as session:, other cleanup

--- models/user.py
+++ models/user.py
@@ -63,40 +63,40 @@ class User(Base):
 
     def get_user_by_id(self, user_id):
         """Lookup user by user_id"""
-        session = Session()
-        user = session.query(User).filter(User.user_id == user_id).first()
-        try:
-            if user:
-                result = user.serialize()
-                return {"success": True, "userinfo": result}
-            return {"success": False, "error": "User not found"}
-        except Exception as e:
-            return {"success": False, "error": str(e)}
+        with Session() as session:
+            user = session.query(User).filter(User.user_id == user_id).first()
+            try:
+                if user:
+                    result = user.serialize()
+                    return {"success": True, "userinfo": result}
+                return {"success": False, "error": "User not found"}
+            except Exception as e:
+                return {"success": False, "error": str(e)}
 
     def get_all_users(self):
         """Get and return usernames and user_ids"""
-        session = Session()
-        try:
-            users = session.query(User.username, User.user_id).all()
-            return {"success": True, "userlist": users}
-        except Exception as e:
-            return {"success": False, "error": str(e)}
+        with Session() as session:
+            try:
+                users = session.query(User.username, User.user_id).all()
+                return {"success": True, "userlist": users}
+            except Exception as e:
+                return {"success": False, "error": str(e)}
 
     def lookup_username(self, username):
         """Get and return user info from username"""
-        session = Session()
-        user = session.query(User).filter(User.username == username).first()
-        try:
-            if user:
-                result = user.serialize()
-                return {"success": True, "userinfo": result}
-            return {"success": False, "error": "User not found"}
-        except Exception as e:
-            return {"success": False, "error": str(e)}
+        with Session() as session:
+            user = session.query(User).filter(User.username == username).first()
+            try:
+                if user:
+                    result = user.serialize()
+                    return {"success": True, "userinfo": result}
+                return {"success": False, "error": "User not found"}
+            except Exception as e:
+                return {"success": False, "error": str(e)}
 
     def _lookup_user(self, userinfo: dict, serialize: bool = False) -> dict:
-        try:
-            with Session() as session:
+        with Session() as session:
+            try:
                 user_id = userinfo.get('user_id')
                 username = userinfo.get('username')
                 if user_id:
@@ -105,51 +105,49 @@ class User(Base):
                     user = session.query(User).filter(User.username == username).first()
                 else:
                     return {"success": False, "error": "No username or user_id provided for lookup"}
-                if user:
-                    if serialize:
-                        return {"success": True, "userinfo": user.serialize()}
+                if user and serialize:
+                    return {"success": True, "userinfo": user.serialize()}
+                elif user:
                     return {"success": True, "userinfo": user}
                 return {"success": False, "error": "User not found"}
-        except Exception as e:
-            return {"success": False, "error": f"error: {str(e)}"}
+            except Exception as e:
+                return {"success": False, "error": f"error: {str(e)}"}
 
     def adduser(self, userinfo):
         """Add user to database"""
         if 'username' not in userinfo or 'nickname' not in userinfo:
             return {"success": False, "error": "Missing username or nickname"}
-
         new_user = User()
         for key, val in userinfo.items():
             if key in user_column_list and key not in protected_create_columns:
                 setattr(new_user, key, val)
-
-        session = Session()
-        try:
-            session.add(new_user)
-            session.commit()
-            return {"success": True, "userinfo": new_user.serialize()}
-        except Exception as e:
-            session.rollback()
-            return {"success": False, "error": str(e)}
+        with Session() as session:
+            try:
+                session.add(new_user)
+                session.commit()
+                return {"success": True, "userinfo": new_user.serialize()}
+            except Exception as e:
+                session.rollback()
+                return {"success": False, "error": str(e)}
 
     def deluser(self, userinfo):
         """Delete user"""
-        session = Session()
-        user_to_delete = None
-        if 'username' in userinfo:
-            user_to_delete = session.query(User).filter(
-                User.username == userinfo['username']).first()
-        elif 'user_id' in userinfo.keys():
-            user_to_delete = session.query(User).filter(
-                User.user_id == userinfo['user_id']).first()
-        if user_to_delete is None:
-            return {"success": False, "error": "User not found"}
-        session.delete(user_to_delete)
-        try:
-            session.commit()
-            return {"success": True}
-        except Exception as e:
-            return {"success": False, "error": str(e)}
+        with Session() as session:
+            user_to_delete = None
+            if 'username' in userinfo:
+                user_to_delete = session.query(User).filter(
+                    User.username == userinfo['username']).first()
+            elif 'user_id' in userinfo.keys():
+                user_to_delete = session.query(User).filter(
+                    User.user_id == userinfo['user_id']).first()
+            if user_to_delete is None:
+                return {"success": False, "error": "User not found"}
+            session.delete(user_to_delete)
+            try:
+                session.commit()
+                return {"success": True}
+            except Exception as e:
+                return {"success": False, "error": str(e)}
 
     def updateuser(self, userinfo):
         """Update user information, except username and protected columns"""
@@ -157,26 +155,24 @@ class User(Base):
         result = self._lookup_user(userinfo)
         if not result['success']:
             return {'success': False, "error": "User not found"}
+        user = result.get('userinfo')
+        user_id = getattr(user, 'user_id')
         with Session() as session:
             user = None
-            if result['success']:
-                user = result.get('userinfo')
-                user_id = getattr(user, 'user_id')
-                for key, val in userinfo.items():
-                    error_keys = []
-                    if key in unprotected_update_columns:
-                        update_object[key] = val
-                    elif key != 'username':
-                        error_keys.append(key)
-                if len(error_keys) > 0:
-                    return {"success": False, "error": f"Invalid key(s): {', '.join(error_keys)}"}
-                if len(update_object) == 0:
-                    return {"success": False, "error": "Insufficient data provided"}
-                try:
-                    session.query(User).filter(User.user_id == user_id).update(update_object)
-                    confirmed_result = session.query(User).filter(User.user_id == user_id).first()
-                    session.commit()
-                    return {"success": True, "userinfo": confirmed_result.serialize()}
-                except Exception as e:
-                    return {"success": False, "error": str(e)}
-        return result
+            for key, val in userinfo.items():
+                error_keys = []
+                if key in unprotected_update_columns:
+                    update_object[key] = val
+                elif key != 'username':
+                    error_keys.append(key)
+            if len(error_keys) > 0:
+                return {"success": False, "error": f"Invalid key(s): {', '.join(error_keys)}"}
+            if len(update_object) == 0:
+                return {"success": False, "error": "Insufficient data provided"}
+            try:
+                session.query(User).filter(User.user_id == user_id).update(update_object)
+                confirmed_result = session.query(User).filter(User.user_id == user_id).first()
+                session.commit()
+                return {"success": True, "userinfo": confirmed_result.serialize()}
+            except Exception as e:
+                return {"success": False, "error": str(e)}
