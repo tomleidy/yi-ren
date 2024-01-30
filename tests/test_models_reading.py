f5e1983a4e27b27f1be5c626b2c816a902e9a3e2
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-01-30 13:23:52 -0800

    tests: the beginning of Reading test cases

---
+++ tests/test_models_reading.py
@@ -0,0 +1,18 @@
+"""Test Reading class methods"""
+from datetime import datetime, timezone
+import pytest
+from faker import Faker
+
+from models.reading import Reading
+# pylint: disable=W0107
+
+
+# TODO: define CRUD operation tests
+# fortunately, readings are simpler than users
+# reader_id, client_id
+# created_at
+# topic
+# reading_notes
+# reading_id
+# hexagram_stationary
+# hexagram_moving
