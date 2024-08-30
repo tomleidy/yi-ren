0c41f1011890e765e0aeb662ea71df460f572b6c
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-08-30 13:34:49 -0400

    chore: remove manual url processing

--- main.js
+++ main.js
@@ -23,31 +23,7 @@ const server = http.createServer((req, res) => {
     let log_line = `${iso_ts()} ${req.method} ${req.url}`
     console.log(log_line);
 
-    if (req.method === 'GET') {
-        let fileUrl = req.url;
-        if (fileUrl === '/') {
-            fileUrl = '/index.html';
-        }
 
-        const filePath = path.resolve('./public' + fileUrl);
-        const fileExt = path.extname(filePath);
-
-        if (fileExt === '.html') {
-            fs.access(filePath, err => {
-                if (err) return send_404(res, `${fileUrl.slice(1)} not found`);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                fs.createReadStream(filePath).pipe(res);
-            });
-        } else {
-            return send_404(res, `${fileUrl} is not an HTML file`);
-        }
-    } else {
-        return send_404(res, `${req.method} not supported`)
-    }
-    // res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/html');
-    // res.end(log_line);
 });
 
 console.log(`Server listening at ${hostname}:${port} ...`)
