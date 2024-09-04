57340be6bcb812844f6052e107bbb0e702b58643
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-04 13:26:43 -0400

    fix: compound indexing, schema name case change

--- node-server/models/hexagram.js
+++ node-server/models/hexagram.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const hexagramSchema = new Schema({
-    "binary": { type: String, immutable: true, index: true },
+const HexagramSchema = new Schema({
+    "lowerBinary": { type: String, immutable: true },
+    "upperBinary": { type: String, immutable: true },
     "king wen": { type: Number, immutable: true, index: true },
     "hanzi": { type: String, immutable: true },
     "pinyin": { type: String, immutable: true },
     "unicode": { type: String, immutable: true },
 })
 
+HexagramSchema.index({ "king wen": 1 });
+HexagramSchema.index({ "lowerBinary": 1, "upperBinary": 1 });
 
-const Hexagram = mongoose.model("Hexagram", hexagramSchema)
+const Hexagram = mongoose.model("Hexagram", HexagramSchema)
 
 module.exports = Hexagram;
