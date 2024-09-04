4a17ab4d2eb0847752f127c0aa6664dbe6a4f0c4
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-04 09:51:58 -0400

    feat: trigram model

---
+++ node-server/models/trigram.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const trigramSchema = new Schema({
+    "binary": { type: String, immutable: true },
+    "fu xi": { type: String, immutable: true },
+    "king wen": { type: String, immutable: true },
+    "pinyin": { type: String, immutable: true },
+    "unicode": { type: String, immutable: true },
+    "english": { type: String, immutable: true }
+})
