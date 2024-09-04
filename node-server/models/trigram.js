7a40d667861222f0dd0aa173e16ec1d04bea6ad9
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-04 09:53:28 -0400

    fix: added forgotten hanzi to trigram schema

--- node-server/models/trigram.js
+++ node-server/models/trigram.js
@@ -5,6 +5,7 @@ const trigramSchema = new Schema({
     "binary": { type: String, immutable: true },
     "fu xi": { type: String, immutable: true },
     "king wen": { type: String, immutable: true },
+    "hanzi": { type: String, immutable: true },
     "pinyin": { type: String, immutable: true },
     "unicode": { type: String, immutable: true },
     "english": { type: String, immutable: true }
