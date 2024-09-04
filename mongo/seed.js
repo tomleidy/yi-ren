5c3e9ba64b7d689233b05f88310ec995e85ff642
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-04 13:14:06 -0400

    fix: trigram binary keys properly named

--- mongo/seed.js
+++ mongo/seed.js
@@ -1,12 +1,12 @@
 db.trigrams.insertMany([
-    { "tbinary": "111", "fu xi": 1, "king wen": 1, "pinyin": "Qián", "hanzi": "乾", "unicode": "☰", "english": "Heaven" },
-    { "tbinary": "011", "fu xi": 7, "king wen": 2, "pinyin": "Duì", "hanzi": "兌", "unicode": "☱", "english": "Lake" },
-    { "tbinary": "101", "fu xi": 6, "king wen": 3, "pinyin": "Lí", "hanzi": "離", "unicode": "☲", "english": "Fire" },
-    { "tbinary": "001", "fu xi": 5, "king wen": 4, "pinyin": "Zhèn", "hanzi": "震", "unicode": "☳", "english": "Thunder" },
-    { "tbinary": "110", "fu xi": 4, "king wen": 5, "pinyin": "Xùn", "hanzi": "巽", "unicode": "☴", "english": "Wind" },
-    { "tbinary": "010", "fu xi": 2, "king wen": 6, "pinyin": "Kǎn", "hanzi": "坎", "unicode": "☵", "english": "Water" },
-    { "tbinary": "100", "fu xi": 3, "king wen": 7, "pinyin": "Gèn", "hanzi": "艮", "unicode": "☶", "english": "Mountain" },
-    { "tbinary": "000", "fu xi": 8, "king wen": 8, "pinyin": "Kūn", "hanzi": "坤", "unicode": "☷", "english": "Earth" }], { ordered: false })
+    { "binary": "111", "fu xi": 1, "king wen": 1, "pinyin": "Qián", "hanzi": "乾", "unicode": "☰", "english": "Heaven" },
+    { "binary": "011", "fu xi": 7, "king wen": 2, "pinyin": "Duì", "hanzi": "兌", "unicode": "☱", "english": "Lake" },
+    { "binary": "101", "fu xi": 6, "king wen": 3, "pinyin": "Lí", "hanzi": "離", "unicode": "☲", "english": "Fire" },
+    { "binary": "001", "fu xi": 5, "king wen": 4, "pinyin": "Zhèn", "hanzi": "震", "unicode": "☳", "english": "Thunder" },
+    { "binary": "110", "fu xi": 4, "king wen": 5, "pinyin": "Xùn", "hanzi": "巽", "unicode": "☴", "english": "Wind" },
+    { "binary": "010", "fu xi": 2, "king wen": 6, "pinyin": "Kǎn", "hanzi": "坎", "unicode": "☵", "english": "Water" },
+    { "binary": "100", "fu xi": 3, "king wen": 7, "pinyin": "Gèn", "hanzi": "艮", "unicode": "☶", "english": "Mountain" },
+    { "binary": "000", "fu xi": 8, "king wen": 8, "pinyin": "Kūn", "hanzi": "坤", "unicode": "☷", "english": "Earth" }], { ordered: false })
 db.trigram.createIndex({ "binary": 1 })
 
 
