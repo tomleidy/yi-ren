d32f778201a659475e3fd7764694db342b8c30ad
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-12 09:35:53 -0400

    refactor: trigram database to be stored client side

--- node-server/models/trigram.js
+++ node-server/models/trigram.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+/* this collection is about 1KB in JSON, it's going to be stored client side */
 
-const trigramSchema = new Schema({
-    "binary": { type: String, immutable: true, required: true, index: true },
-    "fu xi": { type: Number, immutable: true },
-    "king wen": { type: Number, immutable: true },
-    "hanzi": { type: String, immutable: true },
-    "pinyin": { type: String, immutable: true },
-    "unicode": { type: String, immutable: true },
-    "english": { type: String, immutable: true }
-})
 
-const Trigram = mongoose.model("Trigram", trigramSchema)
-
-module.exports = Trigram;
+const binaryToTrigrams = {
+    "111": { "fu xi": "1", "king wen": "1", "pinyin": "Qián", "hanzi": "乾", "unicode": "☰", "english": "Heaven" },
+    "011": { "fu xi": "7", "king wen": "2", "pinyin": "Duì", "hanzi": "兌", "unicode": "☱", "english": "Lake" },
+    "101": { "fu xi": "6", "king wen": "3", "pinyin": "Lí", "hanzi": "離", "unicode": "☲", "english": "Fire" },
+    "001": { "fu xi": "5", "king wen": "4", "pinyin": "Zhèn", "hanzi": "震", "unicode": "☳", "english": "Thunder" },
+    "110": { "fu xi": "4", "king wen": "5", "pinyin": "Xùn", "hanzi": "巽", "unicode": "☴", "english": "Wind" },
+    "010": { "fu xi": "2", "king wen": "6", "pinyin": "Kǎn", "hanzi": "坎", "unicode": "☵", "english": "Water" },
+    "100": { "fu xi": "3", "king wen": "7", "pinyin": "Gèn", "hanzi": "艮", "unicode": "☶", "english": "Mountain" },
+    "000": { "fu xi": "8", "king wen": "8", "pinyin": "Kūn", "hanzi": "坤", "unicode": "☷", "english": "Earth" }
+};
