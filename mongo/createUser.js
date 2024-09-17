e86b84ccd5b4876f134d379e9d5528ebf09a1b5e
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-17 11:09:17 -0400

    feat: create default non-admin user for db

--- mongo/createUser.js
+++ mongo/createUser.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
 
-db.createUser({
+db.userCreate({
   user: process.env.MONGO_YIREN_USERNAME,
   pwd: process.env.MONGO_YIREN_PASSWORD,
   roles: [
     { role: "readWrite", db: process.env.MONGO_YIREN_DBNAME }
   ]
 });
-
