6abe561b664056c16e13e92620a2529cad2edab2
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-09-18 14:42:25 -0400

    fix: reading route paths

--- node-server/routes/reading.js
+++ node-server/routes/reading.js
@@ -4,14 +4,11 @@ const readingRouter = express.Router();
 
 
 
-readingRouter.post("/reading/new/", (req, res) => { });
-// body-parser?
-// verifyPaid, if unpaid: redirect to hexgrams/:hex1[/:hex2]
-// verifyPaid, if paid, add to database then redirect to GET /reading/id/:readingId
-readingRouter.get("/reading/list", (req, res) => { });
-readingRouter.get("/reading/id/:readingId", (req, res) => { });
-readingRouter.put("/reading/id/:readingId", (req, res) => { });
-readingRouter.delete("/reading/id/:readingId", (req, res) => { });
+readingRouter.post("/new", (req, res) => { });
+readingRouter.get("/list", (req, res) => { });
+readingRouter.get("/id/:readingId", (req, res) => { });
+readingRouter.put("/id/:readingId", (req, res) => { });
+readingRouter.delete("/id/:readingId", (req, res) => { });
 
 
 module.exports = readingRouter;
