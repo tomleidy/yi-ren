1791d003adc0311708a4deac320f6349a98305d7
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-10-11 17:36:05 -0400

    refactor: pre-declare and return status/response objects

--- node-server/models/hexagram.js
+++ node-server/models/hexagram.js
@@ -12,18 +12,23 @@ const HexagramSchema = new Schema({
 })
 
 const Hexagram = mongoose.model("Hexagram", HexagramSchema)
+
+const response400InvalidRequest = { status: 400, data: "Invalid hexagram request" };
+const response404Hexagram = { status: 404, data: "Hexagram not found, where did it go?" };
+const response500ServerError = { status: 500, data: "Server error, please try again later" };
+
 let queryOmit = {}
 
 async function lookupHexagrams(hex1, hex2) {
     let lookup1 = getHexagramNumber(hex1);
     let lookup2 = getHexagramNumber(hex2);
+    if (!lookup1 || !lookup2) return response400InvalidRequest;
+
     let queryDoc = { "_id": { $in: [lookup1, lookup2] } };
 
     try {
         const hexagrams = await Hexagram.find(queryDoc, queryOmit)
-        if (!hexagrams || hexagrams.length === 0) {
-            return { status: 404, data: "Hexagrams not found, where did they go?" };
-        }
+        if (!hexagrams || hexagrams.length === 0) return response404Hexagram;
         // reorder the results if the order doesn't match the query
         if (hexagrams[0]["_id"] == lookup2) {
             hexagrams.push(hexagrams.shift());
@@ -31,21 +36,22 @@ async function lookupHexagrams(hex1, hex2) {
         return { status: 200, data: hexagrams };
     }
     catch (err) {
-        return { status: 500, data: err }
+        return response500ServerError;
     }
 };
 
 async function lookupHexagram(hex1) {
+    if (!isValidHexagramString(hex1)) return response400InvalidRequest;
     let queryDoc = { "_id": getHexagramNumber(hex1) };
     try {
         const hexagram = await Hexagram.find(queryDoc, queryOmit)
         if (!hexagram) {
-            return { status: 404, data: "Hexagram not found, where did it go?" };
+            return response404Hexagram;
         }
         return { status: 200, data: hexagram };
     }
     catch (err) {
-        return ({ status: 500, data: err });
+        return response500ServerError;
     }
 }
 
