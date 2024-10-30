7939a737780de8020901eee9c20ccd28d8c971d4
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-10-29 22:56:35 -0400

    feat: implement getReferences helper in model/reference.js

--- node-server/routes/reference.js
+++ node-server/routes/reference.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const createReference = require("../models/reference");
 const { Reference } = require("../models/reference");
+const { getReferences } = require("../helpers/references");
 
 const referenceRouter = express.Router();
 
@@ -10,6 +11,16 @@ referenceRouter.route("/").get(async (req, res, next) => {
     if (result) res.status(200).json(result);
     //let result = await createReference(req.body);
     //res.status(result.status).json(result.data);
+});
+
+referenceRouter.route("/:hexagram").get(async (req, res, next) => {
+    let result = await getReferences(null, [req.params.hexagram]);
+    if (result) res.status(200).json(result);
+});
+
+referenceRouter.route("/:hexagram1/:hexagram2").get(async (req, res, next) => {
+    let result = await getReferences(null, [req.params.hexagram1, req.params.hexagram2]);
+    if (result) res.status(200).json(result);
 });
 
 
