f2ccb01d1b164398b6c18d635f76666348baea8f
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-02 12:52:45 -0400

    refactor: add https and proxy for /references/

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync("../server/https/localhost-key.pem"),
+      cert: fs.readFileSync("../server/https/localhost.pem")
+    },
+    proxy: {
+      "/reference": {
+        target: "https://localhost:443",
+        secure: false
+      },
+    }
+  }
 })
