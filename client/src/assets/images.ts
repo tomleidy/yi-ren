a0cee5c91bd5c8a1756799de4b7321f93204ecf1
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-02 12:54:19 -0400

    refactor: move assets/images and add to images.ts

---
+++ client/src/assets/images.ts
@@ -0,0 +1,13 @@
+import coinHeads from './coins/1935-george-washington-quarter-obverse-768x768.jpg';
+import coinTails from './coins/1935-george-washington-quarter-reverse-768x768.jpg';
+import coinBlended from './coins/1935-quarter-blended.jpg';
+import logoCoollogoCom18186570 from './logo/logo_coollogo_com-18186570.png';
+//import logoImagemagickHires from './logos/logo_imagemagick_hires.png';
+//import logoImagemagickPngcrush from './logos/logo_imagemagick_pngcrush.png';
+
+
+const logo = logoCoollogoCom18186570;
+
+export {
+    coinHeads, coinTails, coinBlended, logo
+};
