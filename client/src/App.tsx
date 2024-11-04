89c9fc9f1c82f1a0dd62c273ada265daae0b730a
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-04 16:51:56 -0500

    refactor: add logo to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import YijingDivination from './components/YijingDivination';
+import { logo } from './assets/images';
 
 function App() {
   return (
-    <div className="App">
-      <YijingDivination />
-    </div>
+    <>
+      <header>
+        <img src={logo} alt="Yi-Ren Logo" />
+      </header>
+
+      <div className="App">
+        <YijingDivination />
+      </div>
+    </>
   );
 }
 export default App
