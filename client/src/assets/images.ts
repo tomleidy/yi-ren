a76397e7ba0e1ab2122316c7b2615f5bfb2d6d50
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-04 17:00:35 -0500

    refactor: use ImageMagick logo and delete previous logo

--- client/src/assets/images.ts
+++ client/src/assets/images.ts
@@ -1,12 +1,11 @@
 import coinHeads from './coins/1935-george-washington-quarter-obverse-768x768.jpg';
 import coinTails from './coins/1935-george-washington-quarter-reverse-768x768.jpg';
 import coinBlended from './coins/1935-quarter-blended.jpg';
-import logoCoollogoCom18186570 from './logo/logo_coollogo_com-18186570.png';
-//import logoImagemagickHires from './logos/logo_imagemagick_hires.png';
-//import logoImagemagickPngcrush from './logos/logo_imagemagick_pngcrush.png';
+//import logoImagemagickHires from './logo/logo_imagemagick_hires.png';
+import logoImagemagickPngcrush from './logo/logo_imagemagick_pngcrush.png';
 
 
-const logo = logoCoollogoCom18186570;
+const logo = logoImagemagickPngcrush;
 
 export {
     coinHeads, coinTails, coinBlended, logo
