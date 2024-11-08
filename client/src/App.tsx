da8b86c5018a231cde8b3a15257bdc82c565fd4d
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-08 00:14:09 -0500

    refactor: move LogoComponent to its own file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import YijingDivination from './components/YijingDivination';
-import { logo } from './assets/images';
 import UserModal from './components/UserModal';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
@@ -7,25 +6,25 @@ import { UserProvider, useUser } from './UserContext';
 import { LogoComponent } from './components/LogoComponent';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
+    return (
         <UserProvider>
-      <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-800">
+            <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-800">
                 <LogoComponent />
                 <Navbar isModalOpen={isModalOpen} toggleModal={() => setIsModalOpen(!isModalOpen)} />
-      </header>
+            </header>
 
-      <UserModal
+            <UserModal
                 isModalOpen={isModalOpen}
                 closeModal={() => setIsModalOpen(false)}
-      />
+            />
 
-      <div className="App">
-        <YijingDivination />
-      </div>
+            <div className="App">
+                <YijingDivination />
+            </div>
         </UserProvider >
-  );
+    );
 }
 
 export default App;
