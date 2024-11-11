929d1092d46ce7a01437a1dc6b376892d9497644
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-11 09:13:43 -0500

    refactor: move getMovingLines to utils/hexagrams.ts

--- client/src/components/yijing.ts
+++ client/src/components/yijing.ts
@@ -1,19 +1,6 @@
-import { getBinaryFromHexagramNumbers } from '../constants/hexagram.js';
-import { HexagramLines, YijingSourceArray } from './types.js';
+import { YijingSourceArray } from './types.js';
 
 
-function getMovingLines(hexagrams: HexagramLines) {
-    const binaryStrings = getBinaryFromHexagramNumbers(hexagrams);
-    const movingLineIndices: number[] = [];
-    if (binaryStrings.length > 1) {
-        for (let i = 0; i < binaryStrings[0].length; i++) {
-            if (binaryStrings[0][i] !== binaryStrings[1][i]) {
-                movingLineIndices.push(i + 1);
-            }
-        }
-    }
-    return movingLineIndices
-}
 
 async function queryYijingTextDbForHexagrams(hexagrams: number[]) {
     const path = `/reference/${hexagrams.join("/")}`;
---
+++ client/src/utils/hexagrams.ts
@@ -0,0 +1,20 @@
+import { getBinaryFromHexagramNumbers } from '../constants/hexagram.js';
+import { HexagramLines } from "../components/types.js";
+
+function getMovingLines(hexagrams: HexagramLines) {
+    const binaryStrings = getBinaryFromHexagramNumbers(hexagrams);
+    const movingLineIndices: number[] = [];
+    if (binaryStrings.length > 1) {
+        for (let i = 0; i < binaryStrings[0].length; i++) {
+            if (binaryStrings[0][i] !== binaryStrings[1][i]) {
+                movingLineIndices.push(i + 1);
+            }
+        }
+    }
+    return movingLineIndices
+}
+
+
+export {
+    getMovingLines
+}
