545dc4b2fc87c9778eb9e934fb6552f8bbeadb87
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2024-11-26 22:26:45 -0500

    refactor: consolidate initialization of mongodb

---
+++ mongo/init.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+
+db = db.getSiblingDB('admin');  // Admin database for root user creation
+
+// Create the root user if it doesn't exist
+if (!db.getUser(process.env.MONGO_INITDB_ROOT_USERNAME)) {
+    db.createUser({
+        user: process.env.MONGO_INITDB_ROOT_USERNAME,
+        pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+        roles: [{ role: 'root', db: 'admin' }]
+    });
+}
+
+// Switch to the YIREN database
+db = db.getSiblingDB(process.env.MONGO_YIREN_DBNAME);
+
+// Create the YIREN user with readWrite role
+
+if (!db.getUser(process.env.MONGO_YIREN_USERNAME)) {
+    db.createUser({
+        user: process.env.MONGO_YIREN_USERNAME,
+        pwd: process.env.MONGO_YIREN_PASSWORD,
+        roles: [{ role: 'readWrite', db: process.env.MONGO_YIREN_DBNAME }]
+    });
+}
+
+
+const existingDoc = db.yijingTexts.findOne({ userId: "000000000000000000000000" });
+if (!existingDoc) {
+    // Read Legge JSON file and insert the data into the yijingtexts collection
+    const leggeText = JSON.parse(fs.readFileSync('/data/db/leggeText.json', 'utf8'));
+    db.yijingTexts.insertMany(leggeText);
+}
